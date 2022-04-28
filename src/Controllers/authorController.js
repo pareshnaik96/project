4cef8798727685bde602b95dfe3b442d28dd8aaa
Animesh Dey <animesh.dey986@gmail.com>
2022-04-28 20:26:47 +0530

    code format

--- src/Controllers/authorController.js
+++ src/Controllers/authorController.js
@@ -5,10 +5,9 @@ let emailRegex = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;  //email validat
 
 let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,15}$/  //password validation
 
-const isValidTitle = function(title){    
+const isValidTitle = function (title) {
     return ['Mr', 'Mrs', 'Miss'].indexOf(title) !== -1     //enum validation
 }
-
 
 // Author Creation
 let createAuthor = async function (req, res) {
@@ -21,16 +20,15 @@ let createAuthor = async function (req, res) {
             if (!isValidTitle(data.title)) return res.status(400).send({ status: false, msg: "Please enter the required field title" })
             if (!data.email) return res.status(400).send({ status: false, msg: "Please enter the required field email" })
             if (!data.password) return res.status(400).send({ status: false, msg: "Please enter the required field password" })
-
-            if (data.fname.length < 2 ) return res.status(400).send({ status: false, msg: "fName length should be min 2" })
+            if (data.fname.length < 2) return res.status(400).send({ status: false, msg: "fName length should be min 2" })
 
             // Email Validation
-            if (!emailRegex.test(data.email))
-                return res.status(400).send({ status: false, msg: "Please provide valid email" })
+            if (!emailRegex.test(data.email)) return res.status(400).send({ status: false, msg: "Please provide valid email" })
+
             // Unique Email
             const usedEmail = await authorModel.findOne({ email: data.email })
-            if (usedEmail)
-                return res.status(400).send({ status: false, msg: "Email Id already exists" })
+            if (usedEmail) return res.status(400).send({ status: false, msg: "Email Id already exists" })
+
             // Password Validation
             if (!passwordRegex.test(data.password))
                 return res.status(400).send({ status: false, msg: "Your password should contain min 6-15 characters, min 1 uppercase, 1 lowercase and 1 special character(@$!%*?&)" })
