8d7153194e7851faef6b4302c0bb534430917a1d
pareshnaik96 <pareshnaik678@gmail.com>
2022-05-03 21:01:42 +0530

    blog

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -11,11 +11,11 @@ router.get("/test-me", function (req, res) {
 //--------------------------------------------------------//
 
 router.post("/authors", authorController.createAuthor)
+router.post("/login", authorController.loginUser)
 router.post("/blogs", middleware.authentication, blogController.createBlog)
 router.get("/blogs", middleware.authentication, blogController.getBlogs)
 router.put("/blogs/:blogId", middleware.authentication, middleware.authorization1, blogController.updateBlogById)
 router.delete("/blogs/:blogId", middleware.authentication, middleware.authorization1, blogController.deleteBlogById)
-router.delete("/blogs", middleware.authentication, middleware.authorization2, blogController.deleteBlogByQuery)
-router.post("/login", authorController.loginUser)
+router.delete("/blogs", middleware.authentication, middleware.authorization2, blogController.deleteBlogByQuery) 
 
 module.exports = router;
